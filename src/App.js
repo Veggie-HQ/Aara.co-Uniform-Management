dd7f058a8936d1c60591606daf2e799dd30626e9
Brihadeeshrk <hello@briha.live>
2023-01-31 01:15:04 +0530

    autorefresh

--- src/App.js
+++ src/App.js
@@ -126,6 +126,10 @@ function App() {
       width: 175,
       windowWidth: 1000,
     });
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 3000);
   };
 
   async function PushOrderToDB(order_details) {
